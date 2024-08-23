a22b2851287b198291ca0be554f633792ff5dc97
Hamsa M <hamsam0097@gmail.com>
2024-08-23 20:21:16 +0100

    finish button update screen func for numbers, begin operator funcs

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 
+function updateScreenNumber() {
+    screenNumber.textContent = firstOperand;
+}
+
+
+const screenNumber = document.getElementsByClassName('screenNumber')[0];
+let selectedNumber;
+let firstOperand = 0;
+let secondOperand = 0;
+let operatorSelection;
+
+const numberButtonsArray = [...document.getElementsByClassName('numbers')];
+const numberButtonsContainer = document.getElementsByClassName('numberContainer')[0];
+
+numberButtonsContainer.addEventListener('click', (e) => {
+    if (e.target.className === 'numbers') {
+        selectedNumber = Number(e.target.id);
+        if (firstOperand === 0) {
+            firstOperand = selectedNumber
+        }
+        else {
+            firstOperand = Number('' + firstOperand + selectedNumber);
+        }
+        
+        updateScreenNumber();
+    }
+    if(e.target.className === 'operators'){
+        //take first operand value as a and if there is no second operand, store operator value and refresh screen for second operand
+        //if equals is clicked, take both operands, operator, and calculate result
+
+    }
+})
+
+//
