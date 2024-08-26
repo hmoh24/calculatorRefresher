1fdc2847694d63d85ed513846d922151bb541283
Hamsa M <hamsam0097@gmail.com>
2024-08-26 13:54:03 +0100

    create functionality for stored number when operator click

--- script.js
+++ script.js
@@ -18,37 +18,86 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 
-function updateScreenNumber() {
-    screenNumber.textContent = firstOperand;
+function updateScreenValues() {
+    screenNumber.textContent = currentNumber;
+    storedNumberElement.textContent = storedNumber;
+
 }
 
 
 const screenNumber = document.getElementsByClassName('screenNumber')[0];
+const storedNumberElement = document.getElementsByClassName('storedNumber')[0];
 let selectedNumber;
-let firstOperand = 0;
-let secondOperand = 0;
-let operatorSelection;
+let currentNumber = '';
+let storedNumber = '';
+let selectedOperator;
 
 const numberButtonsArray = [...document.getElementsByClassName('numbers')];
 const numberButtonsContainer = document.getElementsByClassName('numberContainer')[0];
 
 numberButtonsContainer.addEventListener('click', (e) => {
-    if (e.target.className === 'numbers') {
+    if (e.target.id === 'equal') {
+        if (currentNumber && storedNumber) {
+            currentNumber = selectedOperator(currentNumber, storedNumber)
+        }
+    }
+    else if (e.target.className === 'numbers') {
         selectedNumber = Number(e.target.id);
-        if (firstOperand === 0) {
-            firstOperand = selectedNumber
+        if (currentNumber === 0) {
+            currentNumber = selectedNumber
         }
         else {
-            firstOperand = Number('' + firstOperand + selectedNumber);
+            currentNumber = Number('' + currentNumber + selectedNumber);
         }
         
-        updateScreenNumber();
+        updateScreenValues();
     }
-    if(e.target.className === 'operators'){
-        //take first operand value as a and if there is no second operand, store operator value and refresh screen for second operand
-        //if equals is clicked, take both operands, operator, and calculate result
+    
+})
 
+const operatorContainer = document.getElementsByClassName('operatorContainer')[0];
+
+operatorContainer.addEventListener('click', (e) => {
+    if (e.target.className === 'operators') {
+        switch (e.target.id) {
+            case 'multiply':
+                selectedOperator = multiply;
+                break;
+            case 'divide':
+                selectedOperator = divide;
+                break;
+            case 'subtract':
+                selectedOperator = subtract
+                break;
+            case 'add':
+                selectedOperator = add;
+                break;
+        }
+        console.log(currentNumber);
+        if (currentNumber && !storedNumber) {
+            storedNumber = currentNumber;
+            currentNumber = ''
+            console.log(currentNumber)
+            updateScreenValues();
+        }
+        else if (storedNumber) {
+            currentNumber = selectedOperator(currentNumber, storedNumber)
+            console.log(currentNumber)
+        }
     }
 })
 
-//
+
+
+
+// number is set
+// click operator, set operator value
+// empty current number, and set it as prevnumber
+// set new number, set as current number
+// click equals - evaluate operation according to variables
+
+//edge cases
+//operator clicked first? do nothing
+//number set, equals clicked, return current number as screen number and prev number
+//number set, operator clicked, second number set, operator clicked again
+//this should evaluate first operation, store result as prev value, set new operator, be ready for second value
